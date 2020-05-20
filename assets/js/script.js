9eaf1125c7952104142b5bdd02187ee56922ef85
Jacob Corum <cor15031@byui.edu>
2020-05-20 15:10:11 -0600

    delete and move functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,8 @@ var taskIdCounter = 0;
 var pageContentEl = document.querySelector("#page-content");
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
+var tasksInProgressEl = document.querySelector("#tasks-in-progress");
+var tasksCompletedEl = document.querySelector("#tasks-completed");
 
 var taskFormHandler = function() {
     event.preventDefault();
@@ -14,11 +16,19 @@ var taskFormHandler = function() {
         return false;
     }
     formEl.reset();
-    var taskDataObj = {
-        name: taskNameInput, 
-        type: taskTypeInput
-    };
-    createTaskEl(taskDataObj);
+    var isEdit = formEl.hasAttribute("data-task-id");
+
+    if (isEdit) {
+        var taskId = formEl.getAttribute("data-task-id");
+        completeEditTask(taskNameInput, taskTypeInput, taskId);
+    }
+    else {
+        var dataTaskObj = {
+            name: taskNameInput, 
+            type: taskTypeInput
+        };
+        createTaskEl(dataTaskObj);
+    }
 }
 
 var createTaskEl = function(taskDataObj){
@@ -120,5 +130,39 @@ var editTask = function(taskId) {
     formEl.setAttribute("data-task-id", taskId);
 };
 
+var completeEditTask = function(taskName, taskType, taskId) {
+    // find the matching task list item
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+    
+    // set new values
+    taskSelected.querySelector("h3.task-name").textContent = taskName;
+    taskSelected.querySelector("span.task-type").textContent = taskType;
+    alert("Task Updated!");
+    formEl.removeAttribute("data-task-id");
+    document.querySelector("#save-task").textContent = "Add Task";
+};
+
+var taskStatusChangeHandler = function(event) {
+    // get the task item's id
+    var taskId = event.target.getAttribute("data-task-id");
+
+    //get selected item's value and convert to lowercase
+    var statusValue = event.target.value.toLowerCase();
+
+    // find the parent task item basked on the id
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+
+    if (statusValue === "to do") {
+        tasksToDoEl.appendChild(taskSelected);
+    }
+    else if (statusValue === "in progress") {
+        tasksInProgressEl.appendChild(taskSelected);
+    }
+    else if (statusValue === "completed") {
+        tasksCompletedEl.appendChild(taskSelected);
+    }
+};
+
 formEl.addEventListener("submit", taskFormHandler);
 pageContentEl.addEventListener("click", taskButtonHandler);
+pageContentEl.addEventListener("change", taskStatusChangeHandler);
